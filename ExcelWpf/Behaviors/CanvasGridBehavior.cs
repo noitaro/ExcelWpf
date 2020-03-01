941943cbf28a05fe6d5960336121fa874e6846e2
noitaro <52857466+noitaro@users.noreply.github.com>
2020-03-01 21:21:27 +0900

    Add files via upload

---
+++ ExcelWpf/Behaviors/CanvasGridBehavior.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace ExcelWpf.Behaviors
+{
+    class CanvasGridBehavior : Behavior<Canvas>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.Loaded += AssociatedObject_Loaded;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.Loaded -= AssociatedObject_Loaded;
+        }
+
+        private void AssociatedObject_Loaded(object sender, RoutedEventArgs e)
+        {
+            CreateCanvasGrid();
+        }
+
+        private void CreateCanvasGrid()
+        {
+            for (var x = 0; x < AssociatedObject.ActualWidth / 10; x++)
+            {
+                var geometry = new StreamGeometry
+                {
+                    FillRule = FillRule.EvenOdd
+                };
+
+                using (var ctx = geometry.Open())
+                {
+                    ctx.BeginFigure(new Point(x * 10, 0), false, false);
+                    ctx.LineTo(new Point(x * 10, AssociatedObject.ActualHeight), true, false);
+
+                }
+                geometry.Freeze();
+
+                AssociatedObject.Children.Add(new Path
+                {
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 0.1,
+                    Data = geometry
+                });
+            }
+
+            for (var y = 0; y < AssociatedObject.ActualHeight / 10; y++)
+            {
+                var geometry = new StreamGeometry
+                {
+                    FillRule = FillRule.EvenOdd
+                };
+
+                using (var ctx = geometry.Open())
+                {
+                    ctx.BeginFigure(new Point(0, y * 10), false, false);
+                    ctx.LineTo(new Point(AssociatedObject.ActualWidth, y * 10), true, false);
+
+                }
+                geometry.Freeze();
+
+                AssociatedObject.Children.Add(new Path
+                {
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 0.1,
+                    Data = geometry
+                });
+            }
+        }
+    }
+}
---
+++ ExcelWpf/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace ExcelWpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= MainWindow_Loaded;
+
+            canvas.MouseDown += Canvas_MouseDown;
+            canvas.MouseMove += Canvas_MouseMove;
+            canvas.MouseUp += Canvas_MouseUp;
+
+        }
+
+        bool isMouseDown = false;
+        Point StartPoint;
+        Point MovePoint;
+        Rectangle rectangle;
+
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var point = e.GetPosition(canvas);
+
+            var leftOffset = point.X % 10;
+            leftOffset = leftOffset < 5 ? -leftOffset : 10 - leftOffset;
+            var topOffset = point.Y % 10;
+            topOffset = topOffset < 5 ? -topOffset : 10 - topOffset;
+
+            StartPoint = new Point(point.X + leftOffset, point.Y + topOffset);
+
+            rectangle = new Rectangle
+            {
+                Stroke = Brushes.Black,
+                Fill = Brushes.LightBlue,
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top,
+                Width = 0,
+                Height = 0,
+                Opacity = 0.3
+            };
+
+            Canvas.SetLeft(rectangle, StartPoint.X);
+            Canvas.SetTop(rectangle, StartPoint.Y);
+            canvas.Children.Add(rectangle);
+
+            isMouseDown = true;
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isMouseDown) return;
+
+            var point = e.GetPosition(canvas);
+
+            var leftOffset = point.X % 10;
+            leftOffset = leftOffset < 5 ? -leftOffset : 10 - leftOffset;
+            var topOffset = point.Y % 10;
+            topOffset = topOffset < 5 ? -topOffset : 10 - topOffset;
+
+            MovePoint = new Point(point.X + leftOffset, point.Y + topOffset);
+            rectangle.Width = Math.Abs(MovePoint.X - StartPoint.X);
+            rectangle.Height = Math.Abs(MovePoint.Y - StartPoint.Y);
+
+            if (MovePoint.X - StartPoint.X <= 0)
+            {
+                Canvas.SetLeft(rectangle, MovePoint.X);
+            }
+            if (MovePoint.Y - StartPoint.Y <= 0)
+            {
+                Canvas.SetTop(rectangle, MovePoint.Y);
+            }
+        }
+
+        private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            isMouseDown = false;
+            rectangle.Opacity = 1;
+        }
+
+    }
+}
